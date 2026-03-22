8fc06528df2054cde85147a0ec66ec051b04c2bd
T.sivakumaran <s48002842@gmail.com>
2026-03-22 18:46:24 +0530

    Delete example_secret.py

--- example_secret.py
+++
@@ -1,5 +0,0 @@
-secret = "x9F#kL92mZpQw8VtR4YbN7sHcD3uE1aX"
-aws_key = "AKIA1234567890ABCDEF"
-private_key = """-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASC...
------END PRIVATE KEY-----"""
